13f16cd3ac06c8483868ae23eade69a1b936bbc4
Reyhan Gedik <334480@student.mboutrecht.nl>
2023-03-06 15:05:14 +0100

    our story pagina is klaar

--- app/views/index.php
+++ app/views/index.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= SITENAME ?></title>
 
-    <!-- // «««««««««« Css links »»»»»»»»»» // -->
+    <!-- // «««««««««« Css links »»»»»»»»»» //    components -->
 
-    <link rel="stylesheet" href="../../public/css/components/homepage.css">
+    <link rel="stylesheet" href="../../public/css/about_us.css">
 </head>
 
 <body>
@@ -23,55 +23,27 @@
         <button class="main-button">
             <a href="#" class="button-text">Reserveren</a>
         </button>
-    </div>
-    <br><br>
+    </div><br><br>
 
     <div class="special-offers">
-        <span class="special">Special </span>
-        <span class="offers">Offers</span>
-    </div>
-    <br><br>
+        <span class="special">Our </span>
+        <span class="offers">Story</span>
+    </div><br><br>
 
-    <div class="images">
-        <div class="image">
-            <img src="https://i.lezzet.com.tr/images-xxlarge/cikolatali_sufle-a7a7cc10-0f3c-489c-b18c-82809528580d">
-            <h3>Soufflé</h3>
-            <div class="description">
-                "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                <br><br>
-                <button class="button-style">
-                    <a href="#" class="button-text">Reserveren</a>
-                </button>
-            </div>
-        </div>
-
-        <div class="image">
-            <img src="https://i4.hurimg.com/i/hurriyet/75/750x422/5d4d328f18c7730d14907846.jpg">
-            <h3>Crêpes</h3>
-            <div class="description">
-                "Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                <br><br>
-                <button class="button-style">
-                    <a href="#" class="button-text">Reserveren</a>
-                </button>
-            </div>
+    <div class="left">
+        <img src="https://i.dunya.com/2/1280/720/storage/old/files/2017/11/6/389823/389823.jpg.webp"><br><br><br>
+        <div class="container">
+            <p> <strong> Located in a historical building in Utrecht; Rocambolesque opened its doors in 2023, providing an immersive, multi-sensory dining experience from day one. <br><br> </strong>
+                Two friends, Usem Alaoui and Finn Pinneau, founded the restaurant together. Together they decided to call the restaurant "Rocambolesque". Means "fantastic" in French. Finn's great-grandfather also owned a restaurant of the same name in the north of France. But while they both kept the style of that restaurant, Chef Usem Alaoui's ingenious approach to fine dining thought it would be a good idea to offer a contemporary menu of seasonal dishes from around the world. They decided to work with fixed arrangements and seasonally changing menus to ensure the quality of the food.<br>
+                <br>Welcome to the ultimate destination in your pursuit of pleasure.
+            </p><br><br>
+            <br><br>
         </div>
     </div>
+    <div class="right">
+        <img src="https://lecordonbleu.ozyegin.edu.tr/sites/default/files/uzun-donem_0.png">
+    </div><br>
 
-
-    <div class="restaurant-info ">
-        <img src="https://sc01.alicdn.com/kf/HTB1UZ4sKVXXXXaAaXXXq6xXFXXX2/entry-doors-and-commercial-glass-entry-door.jpg" alt="Restaurant photo">
-        <div class="opening-hours">
-            <h3>OPENING HOURS</h3>
-            <br>
-            <p>MONDAY closed</p>
-            <p>TUESDAY-WEDNESDAY 12:00-00:00</p>
-            <p>THURSDAY-SATURDAY 12:00-02:00</p>
-            <p>SUNDAY 12:00-23:00</p>
-        </div>
-    </div>
-
-    <br>
     <div class="reservation-box">
         <p>You can call us for your reservation requests or you can do it online by clicking the link.</p>
         <div class="button-container">
@@ -79,22 +51,6 @@
             <a href="#" class="contact-button">Contact Us</a>
         </div>
     </div>
-
-    <div class="company">
-        <h2>You're in good company</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div class="image-grid">
-            <img src="https://st3.depositphotos.com/1129232/17148/v/600/depositphotos_171489884-stock-illustration-vector-cartoon-royal-king-burger.jpg" alt="Image 1">
-            <img src="https://static.rfstat.com/renderforest/images/v2/landing-pics/logo_landing/Food/food_logo_2.png" alt="Image 2">
-            <img src="https://static.rfstat.com/renderforest/images/v2/landing-pics/logo_landing/Food/food_logo_3.png" alt="Image 3">
-            <img src="https://static.rfstat.com/renderforest/images/v2/landing-pics/logo_landing/Food/food_logo_4.png" alt="Image 4">
-            <img src="https://static.rfstat.com/renderforest/images/v2/landing-pics/logo_landing/Food/food_logo_5.png" alt="Image 5">
-            <img src="https://logo-suggestion.renderforest.com/suggestions-images/298a/2bbb/298a2bbb7b8724163b713f7ec96b330d.png" alt="Image 6">
-            <img src="https://logo-suggestion.renderforest.com/suggestions-images/0049/c2b9/0049c2b9cf5791ef620e61ac759139d4.png" alt="Image 7">
-            <img src="https://logo-suggestion.renderforest.com/suggestions-images/46e2/3f89/46e23f8956e09846bc18f30d0876f0df.png" alt="Image8">
-        </div>
-    </div>
-
 
 </body>
 
