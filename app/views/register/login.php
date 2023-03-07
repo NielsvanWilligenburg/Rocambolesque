c90c412979041159d12218689555224a69cc3f26
dphoeng <329163@student.mboutrecht.nl>
2023-03-07 15:02:08 +0100

    Registreren pagina is nu responsive

--- app/views/register/login.php
+++ app/views/register/login.php
@@ -7,10 +7,10 @@ if (isset($_SESSION['id']))
 ?>
 
 <body>
-	<div class="head">
+	<div class="head login">
 		<h1>Inloggen</h1>
 	</div>
-	<div class="container">
+	<div class="container login">
 		<div class="row">
 			<div class="col left-side">
 				<img src="../../../public/images/burger.jpg" alt="">
@@ -20,19 +20,21 @@ if (isset($_SESSION['id']))
 			</div>
 			<div class="col right-side">
 				<form action="<?= URLROOT; ?>register/login" method="post" id="loginForm">
-					<div class="form">
+					<div class="form login">
 
-						<div id="username">
+						<div class="subtext" id="username">
 							<input type="text" name="userString" placeholder="Username/Email" maxlength="50" required>
 							<a href="#">Forgot username</a>
 						</div>
-						<div id="password">
+						<div class="subtext" id="password">
 							<input type="password" name="password" placeholder="Password" maxlength="50" required>
 							<a href="#">Forgot password</a>
 						</div>
-						<div id="submit">
-							<p><?= $data['notification'] ?></p>
-							<a class="link-empty submit" onclick="submitForm()">Inloggen</a>
+						<div class="buttons">
+							<div id="submit" class="button">
+								<p class="notification"><?= $data['notification'] ?></p>
+								<a class="link-empty submit" onclick="submitForm()">Inloggen</a>
+							</div>
 						</div>
 					</div>
 				</form>
